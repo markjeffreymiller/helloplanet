bd45533ca94713fa876ea0c2faa1ce96843c3b64
Mark Miller <markjeffreymiller@icloud.com>
2024-04-12 23:44:15 -0700

    Raw output from mvn archetype (dw version 2.1.0)
    name: HelloPlanet
    description: null
    shaded: true
    groupId: com.example.healthchecktest
    artifactId: helloplanet
    version: 1.0-SNAPSHOT
    package: com.example.healthchecktest

---
+++ src/main/java/com/example/healthchecktest/HelloPlanetConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.healthchecktest;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloPlanetConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
